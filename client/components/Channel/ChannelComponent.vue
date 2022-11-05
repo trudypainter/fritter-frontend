d024a2f71436aaf94fca68fae26b1663e7c15d93
trudyvianai <107434432+trudyvianai@users.noreply.github.com>
2022-11-05 09:29:15 -0400

    fixed personal channel page bug

--- client/components/Channel/ChannelComponent.vue
+++ client/components/Channel/ChannelComponent.vue
@@ -139,14 +139,14 @@ export default {
        * Enables edit mode on this freet.
        */
       this.editing = true; // Keeps track of if a freet is being edited
-      this.draft = this.freet.content; // The content of our current "draft" while being edited
+      //   this.draft = this.freet.content; // The content of our current "draft" while being edited
     },
     stopEditing() {
       /**
        * Disables edit mode on this freet.
        */
       this.editing = false;
-      this.draft = this.freet.content;
+      //   this.draft = this.freet.content;
     },
     async deleteFreet() {
       /**
--- client/components/Channel/ChannelPage.vue
+++ client/components/Channel/ChannelPage.vue
@@ -4,7 +4,10 @@
 <template>
   <article class="channel">
     <div class="header">
-      <div class="title">
+      <div v-if="editing">
+        <input v-model="titleInput" placeholder="title" />
+      </div>
+      <div v-else class="title">
         {{ this.channel.title }}
       </div>
       <div class="actions">
@@ -18,7 +21,7 @@
           <div class="button edit" v-if="!editing" @click="startEditing">
             Edit
           </div>
-          <div class="button delete" @click="deleteFreet">Delete</div>
+          <!-- <div class="button delete" @click="deleteFreet">Delete</div> -->
         </div>
         <div v-else>
           <FollowComponent :channelId="`${this.channel._id}`" />
@@ -26,14 +29,19 @@
       </div>
     </div>
     <div class="description">
-      {{ this.channel.description }}
-      <br />
-      <i>Number of Connected Freets: {{ this.connections.length }}</i>
-      <br />
-      by
-      <router-link :to="`/user/${this.channel.author}`"
-        >@{{ this.channel.author }}</router-link
-      >
+      <div v-if="editing">
+        <input v-model="descriptionInput" placeholder="description" />
+      </div>
+      <div v-else>
+        {{ this.channel.description }}
+        <br />
+        <i>Number of Connected Freets: {{ this.connections.length }}</i>
+        <br />
+        by
+        <router-link :to="`/user/${this.channel.author}`"
+          >@{{ this.channel.author }}</router-link
+        >
+      </div>
     </div>
 
     <div v-if="connectionsLoaded" class="connections">
@@ -66,17 +74,17 @@ import FollowComponent from "@/components/Follow/FollowComponent.vue";
 export default {
   name: "ChannelPage",
   components: { ContainerComponent, FollowComponent },
-  props: {
-    // Data from the stored freet
-    channel: {
-      type: Object,
-      required: true,
-    },
-  },
+  // props: {
+  //   // Data from the stored freet
+  //   channel: {
+  //     type: Object,
+  //     required: true,
+  //   },
+  // },
   data() {
     return {
       editing: false, // Whether or not this freet is in edit mode
-      connections: ["tests"], // connections for a freet - init as empty list
+      connections: [], // connections for a freet - init as empty list
       connectionsLoaded: false,
       channel: {},
       titleInput: "",
@@ -112,7 +120,94 @@ export default {
     console.log(resConenctions);
     this.connectionsLoaded = true;
   },
-  methods: {},
+  methods: {
+    startEditing() {
+      /**
+       * Enables edit mode on this freet.
+       */
+      this.editing = true; // Keeps track of if a freet is being edited
+      // this.draft = this.freet.content; // The content of our current "draft" while being edited
+    },
+    stopEditing() {
+      /**
+       * Disables edit mode on this freet.
+       */
+      this.editing = false;
+      // this.draft = this.freet.content;
+    },
+    async deleteFreet() {
+      /**
+       * Deletes this freet.
+       */
+      const params = {
+        method: "DELETE",
+        callback: () => {
+          this.$store.commit("alert", {
+            message: "Successfully deleted channel and all connections.",
+            status: "success",
+          });
+        },
+      };
+      this.request(params);
+    },
+    submitEdit() {
+      /**
+       * Updates freet to have the submitted draft content.
+       */
+      if (this.titleInput === "") {
+        const error = "Error: Title must be at least one character";
+        this.$set(this.alerts, error, "error"); // Set an alert to be the error text, timeout of 3000 ms
+        setTimeout(() => this.$delete(this.alerts, error), 3000);
+        return;
+      }
+
+      const params = {
+        method: "PUT",
+        message: "Successfully edited channel",
+        body: JSON.stringify({
+          title: this.titleInput,
+          description: this.descriptionInput,
+        }),
+        callback: () => {
+          this.$set(this.alerts, params.message, "success");
+          setTimeout(() => this.$delete(this.alerts, params.message), 3000);
+        },
+      };
+      this.request(params);
+      this.channel.title = this.titleInput;
+      this.channel.description = this.descriptionInput;
+    },
+    async request(params) {
+      /**
+       * Submits a request to the freet's endpoint
+       * @param params - Options for the request
+       * @param params.body - Body for the request, if it exists
+       * @param params.callback - Function to run if the the request succeeds
+       */
+      const options = {
+        method: params.method,
+        headers: { "Content-Type": "application/json" },
+      };
+      if (params.body) {
+        options.body = params.body;
+      }
+
+      try {
+        const r = await fetch(`/api/channels/${this.channel._id}`, options);
+        if (!r.ok) {
+          const res = await r.json();
+          throw new Error(res.error);
+        }
+        this.editing = false;
+        this.$store.commit("refreshUserChannels");
+
+        params.callback();
+      } catch (e) {
+        this.$set(this.alerts, e, "error");
+        setTimeout(() => this.$delete(this.alerts, e), 3000);
+      }
+    },
+  },
 };
 </script>
 
@@ -150,5 +245,27 @@ a:hover {
 .actions {
   display: flex;
   position: relative;
+}
+
+.button {
+  margin-left: 13px;
+  line-height: 18px;
+  height: 18px;
+  transform: translateY(12px);
+}
+.button:hover {
+  cursor: pointer;
+  /* text-decoration: underline; */
+}
+
+input {
+  padding: 10px;
+  width: 300px;
+  border: 1px solid black;
+  margin-bottom: 10px;
+}
+input:focus {
+  outline: none;
+  /* border: none; */
 }
 </style>
